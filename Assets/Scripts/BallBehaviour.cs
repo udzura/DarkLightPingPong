231b040dddc8a80ab36957d1c544a40610cefb19
KONDO Uchio <udzura@udzura.jp>
2013-07-17 01:35:36 +0900

    Add ball texture and fix torque vector
    - texture from http://www.flickr.com/photos/horiavarlan/4290845810/

--- Assets/Scripts/BallBehaviour.cs
+++ Assets/Scripts/BallBehaviour.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using System.Collections;
 
 public class BallBehaviour : MonoBehaviour {
-	static Vector3 forwardForce = Vector3.right * 100000000000.0f;
+	static Vector3 forwardForce = Vector3.forward * 1000000000000000.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -11,7 +11,7 @@ public class BallBehaviour : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
 	{
-		var q3 = Quaternion.AngleAxis(Random.Range(-20.0f, 20.0f), Vector3.up);
+		var q3 = Quaternion.AngleAxis(Random.Range(-75.0f, 75.0f), Vector3.up);
 		transform.rigidbody.AddTorque(q3 * forwardForce, ForceMode.VelocityChange);
 		if(transform.position.z < -50 || transform.position.x < -800)
 		{
